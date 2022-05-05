f55a60e7278d877ea3f28f7289697b87563ac467
Roberto Yukio <robertoyukio@outlook.com>
2022-05-05 09:21:43 -0300

    feat: create focus status and keyboard navigation on desktop

--- pages/06-interactive-rating-component/js/script.js
+++ pages/06-interactive-rating-component/js/script.js
@@ -1,16 +1,27 @@
 const ratingForm = document.querySelector(".rating__form");
 const formSubmitted = document.querySelector(".rating__form--submitted");
 const ratings = document.querySelector(".ratings");
+const allRatings = document.querySelectorAll(".ratings__circle");
 const submitBtn = document.querySelector(".btn--submit");
 const message = document.querySelector(".rating__message");
 const finalRating = document.querySelector(".rating__final-rating");
 let ratingValue = 0;
+let keypressed = false;
 
-const handleChange = ({ target }) => {
-  ratingValue = Number(target.getAttribute("data-rating"));
+const handleChange = function (e) {
+  ratingValue = e.target.getAttribute("data-rating");
+  if (keypressed) return;
+  e.target.blur();
 };
 
-const handleSubmit = (e) => {
+const handleKeydown = function (e) {
+  keypressed = e.keyCode === 39 || e.keyCode === 37 ? true : false;
+  setTimeout(() => {
+    keypressed = false;
+  }, 100);
+};
+
+const handleSubmit = function (e) {
   e.preventDefault();
   if (ratingValue === 0) {
     message.classList.add("show");
@@ -30,4 +41,5 @@ const handleSubmit = (e) => {
 };
 
 ratingForm.addEventListener("change", handleChange);
+ratingForm.addEventListener("keydown", handleKeydown);
 ratingForm.addEventListener("submit", handleSubmit);
